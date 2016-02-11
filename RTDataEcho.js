d5de74ca14689087832ca5a68f8cf001d5de2159
twang <xinchenw@hotmail.com>
2016-02-11 17:24:35 +0800

    对DB NEWS里面的两个collection的查询和在web 上显示
    http://127.0.0.1:8080/NEWS/wechat?keyword=%E7%8E%8B%E6%80%9D%E8%81%AA

--- RTDataEcho.js
+++ RTDataEcho.js
@@ -7,17 +7,19 @@ var mport = 27017; //mongo.Connection.DEFAULT_PORT;
 var db = new mongo.Db("NEWS", new mongo.Server(host,mport,{}));
 
 var server = http.createServer(function(req,res){
-	var pathName = req.url;
-	console.log(pathName);
-	var string = "";
+	var pathName = url.parse(req.url).pathname; 
+	var query = url.parse(req.url).query;
+    if (query == undefined) {
+		console.log("Get a null query!")
+	} else {
+	var string = decodeURIComponent(query.substring((query.indexOf("=") +1),query.length));
+	console.log(pathName,"   ",string);
 	switch (pathName){
 		case "/NEWS/domestic" : 
-		     string ="中";
 			 queryString("domestic",string,function(result){
 				 if (!result){
-					 console.log("404 this case");
 					 res.writeHead("404",{"content-type":"text/plain"});
-					 res.write("Sorry no match found");
+					 res.write("Sorry domestic no match found");
 					 res.end();
 				 } else {
 				 res.writeHead("200",{"content-type":"text/html"});
@@ -31,11 +33,30 @@ var server = http.createServer(function(req,res){
 			 });
 			 break;
 			 
+		 case "/NEWS/wechat" : 
+		 queryString("wechat",string,function(result){
+			 if (!result){
+				 res.writeHead("404",{"content-type":"text/plain"});
+				 res.write("Sorry wechat no match found");
+				 res.end();
+			 } else {
+			 res.writeHead("200",{"content-type":"text/html"});
+			 res.write('<head><meta charset="utf-8"/></head>');
+			 result.forEach(function(one){
+		//		 console.log(one.title);
+				 res.write("<b>"+one.title +"</b><br>");
+			 });
+			 res.end();
+			}
+		 });
+		 break;
+			 
 		 default:	
             res.writeHead("404",{"content-type":"text/plain"});
-			res.write("Sorry no match found"); 		 		 
+			res.write("URL does not match any DB collections"); 
+            res.end();			
 	}
-
+  }
 });
 
 server.listen(port,host,function(){
@@ -46,7 +67,7 @@ function queryString(collectionName,string,callback){
 db.open(function(error){
 //	var regex = RegExp("/.*" + string + ".*/");   // ('^' + string)
 //    var query = {'title':regex};
-	var query = {'title': new RegExp('^' + string)};
+	var query = {'title': new RegExp(string)};    // once or more
 	console.log(query);
 	db.collection(collectionName.toString(),function(error,collection){
 		if (error) {
